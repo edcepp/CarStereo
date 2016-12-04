785cce77d066870008f002a50dc4793ccb8f0465
edcepp <epp@zdome.net>
2016-12-03 17:16:19 -0800

    Lab03 implement power button event

--- app/src/main/java/cs371/carstereo/MainActivity.java
+++ app/src/main/java/cs371/carstereo/MainActivity.java
@@ -2,12 +2,27 @@ package cs371.carstereo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean displayOn = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayOn = false;
+    }
+
+    public void powerOn(View view) {
+        TextView channel_text_color = (TextView)findViewById(R.id.channel_display);
+        if (displayOn) {
+            channel_text_color.setTextColor(Color.GREEN);}
+        else {
+            channel_text_color.setTextColor(Color.BLACK);
+        }
+        displayOn = !displayOn;
     }
 }
